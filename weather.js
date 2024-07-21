e0810ae3f9671f41225ea31c063fef4421759a51
Noah Patenaude <you@example.com>
2024-07-21 19:20:26 -0400

    API working

--- weather.js
+++ weather.js
@@ -1,4 +1,3 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
@@ -7,74 +6,48 @@ const apiKey = "";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  let inputVal = input.value;
-
-  //check if there's already a city
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const listItemsArray = Array.from(listItems);
-
-  if (listItemsArray.length > 0) {
-    const filteredArray = listItemsArray.filter(el => {
-      let content = "";
-      //athens,gr
-      if (inputVal.includes(",")) {
-        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-        if (inputVal.split(",")[1].length > 2) {
-          inputVal = inputVal.split(",")[0];
-          content = el
-            .querySelector(".city-name span")
-            .textContent.toLowerCase();
-        } else {
-          content = el.querySelector(".city-name").dataset.name.toLowerCase();
-        }
-      } else {
-        //athens
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-      }
-      return content == inputVal.toLowerCase();
-    });
-
-    if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
-        filteredArray[0].querySelector(".city-name span").textContent
-      } ...otherwise be more specific by providing the country code as well 😉`;
-      form.reset();
-      input.focus();
-      return;
-    }
-  }
-
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const inputVal = input.value;
+  const url = `https://api.weatherapi.com/v1/current.json?q=${inputVal}&lang=english&key=${apiKey}`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]
-      }.svg`;
+      const { location, current } = data;
+      const iconURL = `https:${current.condition.icon}`;
 
       const li = document.createElement("li");
       li.classList.add("city");
       const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
+        <h2 class="city-name" data-name="${location.name},${location.country}">
+          <span>${location.name}</span>
+          <span>${location.country}</span>
         </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]
-      }">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
+
+        <div class="image-temp">
+          <span>${Math.round(current.temp_f)}<sup>°F</sup></span>
+          <figure>
+            <img class="city-icon" src="${iconURL}" alt="${current.condition.text}">
+            <figcaption>${current.condition.text}</figcaption>
+          </figure>
+        </div>
+
+        <div class="additional-info">
+          <p>Region: ${location.region}</p>
+          <p>Local Time: ${location.localtime}</p>
+          <p>Wind: ${current.wind_mph} mph (${current.wind_dir})</p>
+          <p>Pressure: ${current.pressure_in} in</p>
+          <p>Precipitation: ${current.precip_in} in</p>
+          <p>Humidity: ${current.humidity}%</p>
+          <p>Cloud Cover: ${current.cloud}%</p>
+          <p>Feels Like: ${Math.round(current.feelslike_f)}<sup>°C</sup></p>
+          <p>UV Index: ${current.uv}</p>
+        </div>
       `;
       li.innerHTML = markup;
       list.appendChild(li);
     })
     .catch(() => {
-      msg.textContent = "Please search for a valid city 😩";
+      msg.textContent = "Please search for a valid city";
     });
 
   msg.textContent = "";
